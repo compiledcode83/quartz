bbe5632628c7d047226fa36083399215eb56a14a
residentevil0803 <ramseyklassen0803@gmail.com>
2015-08-31 23:53:13 -0400

    chore(karma): execute tests in Chrome

--- karma.conf.js
+++ karma.conf.js
@@ -1,27 +1,16 @@
 module.exports = function(config) {
-
   var options = {
 
     // base path that will be used to resolve all patterns (eg. files, exclude)
     basePath: '.',
 
 
-    // enable / disable watching file and executing tests whenever any file changes
-    autoWatch: false,
-
-
-    // Continuous Integration mode: if true, Karma captures browsers, runs the tests and exits
-    singleRun: true,
-
-
-    // start these browsers
-    // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: process.env.TRAVIS ? ['Firefox'] : ['Chrome'],
-
-
     // frameworks to use
     // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-    frameworks: ['jasmine', 'fixture'],
+    frameworks: [
+      'jasmine',
+      'fixture'
+    ],
 
 
     // list of files / patterns to load in the browser
@@ -47,6 +36,7 @@ module.exports = function(config) {
 
     // test results reporter to use
     // possible values: 'dots', 'progress'
+    // available reporters: https://npmjs.org/browse/keyword/karma-reporter
     reporters: ['dots'],
 
 
@@ -60,13 +50,37 @@ module.exports = function(config) {
 
     // level of logging
     // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
-    logLevel: config.LOG_INFO
+    logLevel: config.LOG_INFO,
+
+
+    // enable / disable watching file and executing tests whenever any file changes
+    autoWatch: true,
+
+
+    // Continuous Integration mode
+    // if true, Karma captures browsers, runs the tests and exits
+    singleRun: false,
+
+
+    // custom launcher for travis-ci
+    customLaunchers: {
+      Chrome_travis_ci: {
+        base: 'Chrome',
+        flags: ['--no-sandbox']
+      }
+    },
+
+
+    // start these browsers
+    // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
+    browsers: process.env.TRAVIS ? ['Chrome_travis_ci'] : ['Chrome']
 
   };
 
 
   // additional options for coverage
   if (process.argv.indexOf('--coverage') !== -1) {
+    options.singleRun = true;
     options.preprocessors['src/quartz.js'] = 'coverage';
     options.reporters.push('coverage');
     options.coverageReporter = {
